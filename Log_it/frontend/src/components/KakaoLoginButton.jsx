84033f7bb3c1f76d9f81cefcf2a6a4e10953f8fc
DESKTOP-H18L79N\user <sunyoeng423@naver.com>
2025-08-29 11:28:00 +0900

    no message

--- Log_it/frontend/src/components/KakaoLoginButton.jsx
+++ Log_it/frontend/src/components/KakaoLoginButton.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const KakaoLoginButton = () => {
   const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
-  const REDIRECT_URI = "http://localhost:4000/api/auth/kakao/callback"; // 백엔드 콜백 주소
+  // const REDIRECT_URI = "http://localhost:4000/api/auth/kakao/callback"; // 백엔드 콜백 주소
+  const REDIRECT_URI = "http://localhost:4000/api/auth/kakao/callback";
 
   const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${encodeURIComponent(
     REDIRECT_URI
