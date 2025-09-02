634d46c114fd8dc297b324d689ab548d72a6ac53
DESKTOP-H18L79N\user <sunyoeng423@naver.com>
2025-09-02 10:45:34 +0900

    no message

--- Log_it/backend/app.js
+++ Log_it/backend/app.js
@@ -44,6 +44,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
