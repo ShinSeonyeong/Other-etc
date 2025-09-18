bef6c8a8b06ee13ba2a7d300ad054a34efb99101
DESKTOP-H18L79N\user <sunyoeng423@naver.com>
2025-09-18 15:34:09 +0900

    pwa - vite버전

--- Log_it/frontend/src/main.jsx
+++ Log_it/frontend/src/main.jsx
@@ -2,19 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register' // PWA 서비스 워커 등록
+
+registerSW()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
-// 서비스워커 등록
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js") // public/sw.js 경로
-      .then(() => console.log("Service Worker 등록 성공"))
-      .catch((error) => console.log("Service Worker 등록 실패", error));
-  });
-}
